50080efe53b704ca3f4d06f06b42ed97a3b15e1e
gaurav <gauravy1905@gmail.com>
2024-04-28 08:34:56 +0530

    New Utility functions added

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-
+import pickle
 import sys
 import os
 
 from .exceptions import CustomException
 from .logger import logging
 
-logging.info("File started")
 
 def calculate_yrs(days:int) -> int:
     years = (days / 365.25)
@@ -17,16 +16,23 @@ def calculate_yrs(days:int) -> int:
     return years
 
 
-
 def save_data(name:str, path:str, df:pd.DataFrame) -> None:
-
     try:
-
         df.to_csv(os.path.join(path, name),
                     index=False)
 
     except Exception as e:
         logging.info("Error occured in saving the model.")
         raise CustomException(e, sys)
+
+
+def save_models(name:str, path:str, model) -> None:
+    try:
+        with open(os.path.join(path,name), 'wb') as f:
+            pickle.dump(model, f)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
 
 
