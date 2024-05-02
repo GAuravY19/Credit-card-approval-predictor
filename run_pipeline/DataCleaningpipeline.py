f5ddb2d23dfcf6887459d61e079cd2d5b249669d
gaurav <gauravy1905@gmail.com>
2024-05-02 10:58:10 +0530

    Converted to class to run the pipeline

--- run_pipeline/DataCleaningpipeline.py
+++ run_pipeline/DataCleaningpipeline.py
@@ -1,26 +1,26 @@
 import pandas as pd
-import numpy as np
 
 from src.main_src.data_cleaning import DataClean
 
-import sys
-
-from src.exceptions import CustomException
 from src.logger import logging
 
-df = pd.read_csv(r'D:\Credit-card-approval-predictor\data\Raw_data\NewData.csv')
-logging.info("Dataset loaded")
+class DatacleanPipeline:
+    def __init__(self):
 
-logging.info("Performing data cleaning.")
+        self.df = pd.read_csv(r'D:\Credit-card-approval-predictor\data\Raw_data\NewData.csv')
+        logging.info("Dataset loaded")
 
-clean = DataClean(df)
-df1 = clean.step_1_Drop_column(df)
-df2 = clean.step_2_cnt_children(df1)
-df3 = clean.step_3_amt_income_total(df2)
-df4 = clean.step_4_days_birth(df3)
-df5 = clean.step_5_days_emp(df4)
-df6 = clean.step_6_occupation_type(df5)
-clean.step_7_save_model(df6)
+        logging.info("Performing data cleaning.")
+        self.clean = DataClean(self.df)
 
-logging.info("Datacleaning Completed.")
+    def RunningMethods(self):
+        df1 = self.clean.step_1_Drop_column(self.df)
+        df2 = self.clean.step_2_cnt_children(df1)
+        df3 = self.clean.step_3_amt_income_total(df2)
+        df4 = self.clean.step_4_days_birth(df3)
+        df5 = self.clean.step_5_days_emp(df4)
+        df6 = self.clean.step_6_occupation_type(df5)
+        self.clean.step_7_save_model(df6)
+
+        logging.info("Datacleaning Completed.")
 
